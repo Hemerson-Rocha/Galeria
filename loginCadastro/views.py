fa202b83c42fa5c8ede5042357050f248fec4228
Hemerson-Rocha <hemerson.me@gmail.com>
2022-12-16 20:27:34 -0300

    iniciando sistema de login

--- loginCadastro/views.py
+++ loginCadastro/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        email_user = request.POST.get('email')
+        password_user = request.POST.get('password')
+        print(f'email == {email_user}\npassword == {password_user}')
+    return render(request, 'tela-login.html')
