c9cef71118eee83dc10ee2b82408a1057524e1b0
Hemerson-Rocha <hemerson.me@gmail.com>
2022-12-17 22:16:28 -0300

    tentativa de dropdown...

--- loginCadastro/views.py
+++ loginCadastro/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
@@ -9,4 +12,17 @@ def login(request):
         email_user = request.POST.get('email')
         password_user = request.POST.get('password')
         print(f'email == {email_user}\npassword == {password_user}')
+
+    if User.objects.filter(email=email_user).exists():
+        nome = User.objects.filter(email=email_user).values_list('username', flat=True).get()
+        user = authenticate(username=nome, password=password_user )
+        if user:
+            print("autenticado")
+            login_django(request, user)
+        else:
+            print("Email ou senha incorretos")
+        print(f" resultado do user: {user} \nresultado do nome: {nome}")
+    else:
+        print("Email ou senha incorretos")
+
     return render(request, 'tela-login.html')
